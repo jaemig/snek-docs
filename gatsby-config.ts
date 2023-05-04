e094365fe2e4bcd74636eb6582e64750052cc0f5
Jan-Emig <jan.emig@pm.me>
2023-05-04 23:08:54 +0200

    Init commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,16 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    {
+      resolve: '@chakra-ui/gatsby-plugin',
+      options: {
+        resetCSS: true,
+        isUsingColorMode: true,
+        isBaseProvider: false,
+      },
+    },
+  ],
 }
 
 export default config
