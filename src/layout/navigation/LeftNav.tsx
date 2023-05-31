ab700536e7072ecc19421bc1e97633d8057132ce
Nico Schett <nicoschett@icloud.com>
2023-05-31 17:27:34 +0200

    refactor: reformat with default prettier config

--- src/layout/navigation/LeftNav.tsx
+++ src/layout/navigation/LeftNav.tsx
@@ -7,26 +7,26 @@ import NavbarControls from "./components/NavbarControls";
  * Left navigation bar.
  */
 const LeftNav: FC = () => {
-    const [isExpanded, setIsExpanded] = useState(true);
+  const [isExpanded, setIsExpanded] = useState(true);
 
-    return (
-        <Flex
-            position='sticky'
-            top='80px'
-            as='nav'
-            fontSize='sm'
-            flexDirection='column'
-            h='calc(100vh - 100px)'
-            w={isExpanded ? 'auto' : '5rem'}
-            color='shared.text.default'
-        >
-            <Box w={isExpanded ? 'auto' : 0} >
-                <PageDirectory isExpanded={isExpanded} />
-            </Box>
-            <Spacer />
-            <NavbarControls isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
-        </Flex>
-    )
-}
+  return (
+    <Flex
+      position="sticky"
+      top="80px"
+      as="nav"
+      fontSize="sm"
+      flexDirection="column"
+      h="calc(100vh - 100px)"
+      w={isExpanded ? "auto" : "5rem"}
+      color="shared.text.default"
+    >
+      <Box w={isExpanded ? "auto" : 0}>
+        <PageDirectory isExpanded={isExpanded} />
+      </Box>
+      <Spacer />
+      <NavbarControls isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
+    </Flex>
+  );
+};
 
-export default LeftNav
+export default LeftNav;
