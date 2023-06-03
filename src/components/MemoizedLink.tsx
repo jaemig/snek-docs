ad77e2947b5a7ed156e315daef1c8b626e57569d
Jan-Emig <jan.emig@pm.me>
2023-06-03 21:44:40 +0200

    fix(memoizedLink): Usage of correct link component
    The MemoizedLink now uses the custom Link component under the hood. This should fix the issue that some of the links are not proper Gatsby links.

--- src/components/MemoizedLink.tsx
+++ src/components/MemoizedLink.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
-import { LinkData } from '../layout/navigation/navigation.types';
-import { Link, LinkProps } from '@chakra-ui/react';
+import { LinkProps } from '@chakra-ui/react';
+import { LinkData } from '../types/navigation';
+import Link from './Link';
 
 interface LinksProps {
   links: LinkData[];
@@ -17,8 +18,7 @@ const Links: FC<LinksProps> = ({ links, props, activeProps }) => {
             key={link.name}
             href={link.href}
             {...props}
-            {...(link.isActive ? activeProps : {})}
-          >
+            {...(link.isActive ? activeProps : {})}>
             {link.name}
           </Link>
         );
