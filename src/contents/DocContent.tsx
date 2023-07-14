9482a2892d56879fa8fb242402740dcf404e6cbb
Jan-Emig <jan.emig@pm.me>
2023-07-15 01:00:48 +0200

    feat(rightNav): Outsourced children
    This outsources the children away from the right nav, allowing the component to be used more dynamically.

--- src/contents/DocContent.tsx
+++ src/contents/DocContent.tsx
@@ -1,6 +1,18 @@
-import { Box, Stack, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Stack,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  VStack,
+  Text as ChText
+} from '@chakra-ui/react';
 import { Field } from '@snek-at/jaen';
-import React from 'react';
+import React, { memo } from 'react';
 
 import { useNavOffset } from '../hooks/use-nav-offset';
 import RightNav from '../layout/navigation/RightNav';
@@ -20,11 +32,28 @@ import ImageCard from '../components/main-content/image-card/ImageCard';
 import Callout from '../components/main-content/callout/Callouts';
 import IconCard from '../components/main-content/icon-card/IconCard';
 import CodePlayground from '../components/main-content/code-playground/CodePlayground';
+import MemoizedLinks from '../components/core/MemoizedLink';
+import TableOfContent from '../layout/navigation/components/TableOfContent';
+
+// Example links - these would probably be fetched from a CMS or other data source
+const links = [
+  {
+    name: 'Question? Give us feedback',
+    href: 'https://snek.at/'
+  },
+  {
+    name: 'Edit this page on Jaen',
+    href: '/admin/#/pages'
+  }
+];
 
 export interface DocContentProps {}
 
 export const DocContent: React.FC<DocContentProps> = () => {
   const navTopOffset = useNavOffset();
+
+  // This can be memoized since it doesn't change and switching pages re-renders most of the app anyway.
+  const MemoizedToc = memo(TableOfContent, () => false);
 
   return (
     <Stack spacing={{ base: 0, xl: 12 }} direction="row">
@@ -107,7 +136,32 @@ export const DocContent: React.FC<DocContentProps> = () => {
       </Box>
 
       <Box position="sticky" top={`calc(0px + ${navTopOffset})`}>
-        <RightNav />
+        <RightNav>
+          <ChText color="rightNav.titleTop.color" fontWeight="semibold">
+            On This Page
+          </ChText>
+          <Flex as="nav" direction="column" mt={5}>
+            <MemoizedToc />
+          </Flex>
+          <Box
+            mt={7}
+            pt={7}
+            borderTop="1px solid"
+            borderTopColor="components.separator.borderColor"
+            fontSize="xs"
+          >
+            <VStack rowGap={1} textAlign="left">
+              <MemoizedLinks
+                links={links}
+                props={{
+                  variant: 'right-bottom-nav',
+                  w: '100%',
+                  display: 'block'
+                }}
+              />
+            </VStack>
+          </Box>
+        </RightNav>
       </Box>
     </Stack>
   );
--- src/layout/navigation/RightNav.tsx
+++ src/layout/navigation/RightNav.tsx
@@ -1,27 +1,17 @@
-import React, { FC, memo } from 'react';
+import React, { FC, ReactNode, memo } from 'react';
 import { Box, Flex, Text, VStack } from '@chakra-ui/react';
 import MemoizedLinks from '../../components/core/MemoizedLink';
 import TableOfContent from './components/TableOfContent';
 import { useNavOffset } from '../../hooks/use-nav-offset';
 
-// Example links - these would probably be fetched from a CMS or other data source
-const links = [
-  {
-    name: 'Question? Give us feedback',
-    href: 'https://snek.at/'
-  },
-  {
-    name: 'Edit this page on Jaen',
-    href: '/admin/#/pages'
-  }
-];
+interface IRightNavProps {
+  children?: ReactNode;
+}
 
 /**
  * Right navigation bar.
  */
-const RightNav: FC = ({}) => {
-  // This can be memoized since it doesn't change and switching pages re-renders most of the app anyway.
-  const MemoizedToc = memo(TableOfContent, () => false);
+const RightNav: FC<IRightNavProps> = ({ children }) => {
   const navTopOffset = useNavOffset();
 
   return (
@@ -33,30 +23,7 @@ const RightNav: FC = ({}) => {
       color="shared.text.default"
       fontSize="sm"
     >
-      <Text color="rightNav.titleTop.color" fontWeight="semibold">
-        On This Page
-      </Text>
-      <Flex as="nav" direction="column" mt={5}>
-        <MemoizedToc />
-      </Flex>
-      <Box
-        mt={7}
-        pt={7}
-        borderTop="1px solid"
-        borderTopColor="components.separator.borderColor"
-        fontSize="xs"
-      >
-        <VStack rowGap={1} textAlign="left">
-          <MemoizedLinks
-            links={links}
-            props={{
-              variant: 'right-bottom-nav',
-              w: '100%',
-              display: 'block'
-            }}
-          />
-        </VStack>
-      </Box>
+      {children}
     </Box>
   );
 };
