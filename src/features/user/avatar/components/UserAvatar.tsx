233f5c59acf71ca643be89cef4fac3b0e55b90bf
Jan-Emig <jan.emig@pm.me>
2023-08-20 01:08:46 +0200

    feat(userAvatar): Extended features

--- src/features/user/avatar/components/UserAvatar.tsx
+++ src/features/user/avatar/components/UserAvatar.tsx
@@ -1,20 +1,60 @@
 import { FC } from 'react';
 import { TUser } from '../../types/user';
-import { Avatar, AvatarProps, Tooltip } from '@chakra-ui/react';
+import {
+  Avatar,
+  AvatarProps,
+  LinkBox,
+  LinkOverlay,
+  Tooltip
+} from '@chakra-ui/react';
 import UserPreview from './UserPreview';
 
 export interface IUserAvatarProps extends AvatarProps {
   user: TUser;
   showTooltip?: boolean;
+  redirectToProfile?: boolean;
+  scaleOnHover?: boolean;
 }
 
-const UserAvatar: FC<IUserAvatarProps> = ({ user, showTooltip, ...props }) => {
-  const avatar = (
-    <Avatar
-      src={user.avatarUrl ?? 'https://api.dicebear.com/6.x/thumbs/svg'}
-      {...props}
-    />
-  );
+const UserAvatar: FC<IUserAvatarProps> = ({
+  user,
+  showTooltip,
+  redirectToProfile,
+  scaleOnHover,
+  ...props
+}) => {
+  const imgSrc = user.avatarUrl ?? 'https://api.dicebear.com/6.x/thumbs/svg';
+  let avatarProps: AvatarProps = {};
+
+  if (scaleOnHover) {
+    avatarProps._hover = {
+      ...avatarProps._hover,
+      transform: 'scale(1.1)'
+    };
+    avatarProps.transition = 'transform 0.2s ease-in-out';
+  }
+
+  avatarProps = {
+    ...avatarProps,
+    ...props,
+    _hover: { ...avatarProps._hover, ...props._hover } // merge hover props properly
+  };
+
+  let avatar: JSX.Element;
+  if (redirectToProfile) {
+    avatar = (
+      <LinkBox as={Avatar} src={imgSrc} {...avatarProps}>
+        <LinkOverlay href="/profile" />
+      </LinkBox>
+    );
+  } else {
+    avatar = (
+      <Avatar
+        src={user.avatarUrl ?? 'https://api.dicebear.com/6.x/thumbs/svg'}
+        {...avatarProps}
+      />
+    );
+  }
 
   if (showTooltip) {
     return (
